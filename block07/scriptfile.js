aa9da22e28cbe7d2acd9cfea79b61ac3014541c6
Jenova <141297009+jenovayt@users.noreply.github.com>
2024-09-19 16:20:02 -0500

    Add files via upload

---
+++ block07/scriptfile.js
@@ -0,0 +1,15 @@
+// I use window alert here to let the user know what they have to do.
+window.alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination:")
+
+// I define these variable as what they are, and I use the console log to show the final results in the console of the page.
+let firstSum = 6 + 4;
+
+console.log(firstSum)
+
+let firstProduct = firstSum * 4;
+
+console.log(firstProduct)
+
+let secondProduct = firstProduct - 1;
+
+console.log(secondProduct)
